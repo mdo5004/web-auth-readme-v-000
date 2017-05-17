9f46d9ef76d915f44230f5a88f35a6cca1beb110
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-17 09:31:42 -0400

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,31 +1,37 @@
 class SearchesController < ApplicationController
-  def search
-  end
-
-  def foursquare
-
-    client_id = ENV['FOURSQUARE_CLIENT_ID']
-    client_secret = ENV['FOURSQUARE_SECRET']
-
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-      req.params['client_id'] = client_id
-      req.params['client_secret'] = client_secret
-      req.params['v'] = '20160201'
-      req.params['near'] = params[:zipcode]
-      req.params['query'] = 'coffee shop'
+    def search
     end
-
-    body = JSON.parse(@resp.body)
-
-    if @resp.success?
-      @venues = body["response"]["venues"]
-    else
-      @error = body["meta"]["errorDetail"]
+    def friends
+        resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+            req.params['oauth_token'] = session[:token]
+            req.params['v'] = '20160201'
+        end
+        @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
     end
-    render 'search'
+    def foursquare
+
+        client_id = ENV['FOURSQUARE_CLIENT_ID']
+        client_secret = ENV['FOURSQUARE_SECRET']
+
+        @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+            req.params['client_id'] = client_id
+            req.params['client_secret'] = client_secret
+            req.params['v'] = '20160201'
+            req.params['near'] = params[:zipcode]
+            req.params['query'] = 'coffee shop'
+        end
+
+        body = JSON.parse(@resp.body)
+
+        if @resp.success?
+            @venues = body["response"]["venues"]
+        else
+            @error = body["meta"]["errorDetail"]
+        end
+        render 'search'
 
     rescue Faraday::TimeoutError
-      @error = "There was a timeout. Please try again."
-      render 'search'
-  end
+        @error = "There was a timeout. Please try again."
+        render 'search'
+    end
 end
